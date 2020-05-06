67311d56dee46ffc1a61ed51aa314ed245524f1f
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-06 11:32:48 -0600

    init

---
+++ Interview Cake/Hashing and hash tables/populateWordsToCounts.js
@@ -0,0 +1,14 @@
+class WordCloudData {
+  constructor(inputString) {
+    this.wordsToCounts = new Map();
+    this.populateWordsToCounts(inputString);
+  }
+
+  populateWordsToCounts(inputString) {
+
+    // Count the frequency of each word
+
+
+  }
+
+}
