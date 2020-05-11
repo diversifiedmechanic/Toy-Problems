52a01865fd8ea58a7156dc24621568d55b55c231
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-11 12:47:27 -0600

    Psuedocode

--- LeetCode/May/isBadVersion.js
+++ LeetCode/May/isBadVersion.js
@@ -36,11 +36,26 @@ var solution = function(isBadVersion) {
    * @return {integer} The first bad version
    */
   return function(n) {
-    // find the middle
     // track the left idx
     // track the right idx
+    // define current val
+    // tuple of last known good and last known bad
 
+    // while the there is a value between right and left
+      // set the current value as the middle of right and left
 
+      // if the current value is bad
+        // pull right idx to current idx
+        // set last known bad to current idx
+
+      // otherwise
+        // set the current left idx to current idx
+        // set the last known good to current idx
+
+      // if last known good and last known bad are one index away
+        // return the value at the last known bad idx
+
+    // return null
 
   };
 };
