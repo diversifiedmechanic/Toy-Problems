728a6f236ba1dc69a7b8e07765f323fc0d6aaa76
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-18 15:48:00 -0600

    First pass w/out tests.

--- LeetCode/May/Week 3/checkInclusion.js
+++ LeetCode/May/Week 3/checkInclusion.js
@@ -22,20 +22,52 @@ Note:
  */
 
 var checkInclusion = function(s1, s2) {
-  // store s1 in an object
+  let store = {};
 
-  // iterate through the string
+  // store s1 in an object
+  for (let l of s1) {
+    if (store[l] === undefined) {
+      store[l] = 1;
+    } else {
+      store[l] += 1;
+    }
+  }
+
+  // iterate through the second string
+  for (let i = 0; i < s2.length; i++) {
     // if the current letter is in the obj
+    if (store[s2[i]]) {
       // copy the object of letters
+      let storeCopy = {...store};
+      let idx = i;
+
       // while the letter exists in the obj
+      while (storeCopy[s2[i]] !== undefined) {
         // subtract one from the letter count
+        storeCopy[s2[i]] -= 1;
+
         // if the letter count becomes 0
+        if (storeCopy[s2[i]] === 0) {
           // delete the letter from the obj
+          delete storeCopy[s2[i]];
+        }
 
         // if the object has run out of letters
+        if (!Object.keys(storeCopy).length) {
           // return true
+          return true;
+        }
+
         // increment index by one
+        idx += 1;
+      }
+
       // current letter = index from while loop
+      i = idx;
+    }
+  }
 
   return false;
 };
+
+
