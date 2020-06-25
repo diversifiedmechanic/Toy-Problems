0719064a7df8a99240468174fd3d5341b46034d6
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-06-25 10:04:58 -0600

    Init with first half of code.

---
+++ LeetCode/June/combinationSum.js
@@ -0,0 +1,55 @@
+/*
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+Note:
+
+  All numbers (including target) will be positive integers.
+  The solution set must not contain duplicate combinations.
+
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+
+Example 2:
+
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+*/
+
+/*
+* @param {number[]} candidates
+* @param {number} target
+* @return {number[][]}
+*/
+
+var combinationSum = function(candidates, target) {
+  let result = [];
+
+  let sorted = candidates.sort((a, b) => b - a);
+
+  let buildCombinations = (idx, combination, combinationTotal) => {
+    if (combinationTotal === target) return result.push(combination);
+    // if (combinationTotal > target) return;
+
+    for (let i = idx; i < sorted.length; i++) {
+      if (combinationTotal + sorted[i] <= target)
+    }
+
+  }
+
+  buildCombinations(0, [])
+
+  return result;
+};
