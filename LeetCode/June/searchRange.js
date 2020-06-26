f3a60874dd46d3c0fe583f0d6c82f6c946159ddf
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-06-26 12:18:51 -0600

    Solving all tests.

--- LeetCode/June/searchRange.js
+++ LeetCode/June/searchRange.js
@@ -9,12 +9,10 @@ Your algorithm's runtime complexity must be in the order of O(log n).
 If the target is not found in the array, return [-1, -1].
 
 Example 1:
-
 Input: nums = [5,7,7,8,8,10], target = 8
 Output: [3,4]
 
 Example 2:
-
 Input: nums = [5,7,7,8,8,10], target = 6
 Output: [-1,-1]
 */
@@ -30,6 +28,7 @@ var findRange = (nums, targetValue, targetIdx) => {
   let endIdx = targetIdx;
 
   let range = [];
+
   while (nums[startIdx] === targetValue) {
     range[0] = startIdx;
     startIdx -= 1;
@@ -37,8 +36,10 @@ var findRange = (nums, targetValue, targetIdx) => {
 
   while (nums[endIdx] === targetValue) {
     range[1] = endIdx;
-    startIdx += 1;
+    endIdx += 1;
   }
+
+  return range;
 }
 
 var searchRange = function(nums, target) {
@@ -46,7 +47,7 @@ var searchRange = function(nums, target) {
   let leftIdx = 0;
 
   while (rightIdx - leftIdx >= 0) {
-    let middleIdx = (rightIdx + leftIdx) / 2;
+    let middleIdx = Math.floor((rightIdx + leftIdx) / 2);
 
     if (nums[middleIdx] === target) {
       return findRange(nums, target, middleIdx);
@@ -61,3 +62,17 @@ var searchRange = function(nums, target) {
 
   return [-1, -1];
 };
+
+/////////////////// TESTS //////////////////////
+
+// should find the range when there is one to find
+console.log(JSON.stringify(searchRange([5,7,7,8,8,10], 8)) === JSON.stringify([3,4]));
+
+// should return the same start and end when there is only one element that matches the target
+console.log(JSON.stringify(searchRange([5,7,7,8,8,10], 5)) === JSON.stringify([0,0]));
+
+// should return the whole range if there are several matching elements
+console.log(JSON.stringify(searchRange([5,7,7,7,7,7,8,8,10], 7)) === JSON.stringify([1,5]));
+
+// should return [-1, -1] when the target is not in the array
+console.log(JSON.stringify(searchRange([5,7,7,8,8,10], 6)) === JSON.stringify([-1, -1]));
