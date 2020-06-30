32343a665a26fbf18691952224319c53b473c118
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-06-30 08:34:47 -0600

    init

---
+++ LeetCode/June/permutations.js
@@ -0,0 +1,24 @@
+/*
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+
+/*
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var permute = function(nums) {
+
+};
