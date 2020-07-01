41e429b71cda65a0c0f2d76f0f41fe96f9e73f98
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-01 14:00:09 -0600

    First pass without tests.

--- Interview Cake/Greedy/highestProductOfThree.js
+++ Interview Cake/Greedy/highestProductOfThree.js
@@ -4,6 +4,30 @@ The input arrayOfInts will always have at least three integers.
 */
 
 function highestProductOf3(arrayOfInts) {
+  let positiveThree = [0, 0, 0];
+  let negativeTwo = [0, 0];
 
+  for (let i = 0; i < arrayOfInts.length; i++) {
+    if (arrayOfInts[i] >= positiveThree[0]) {
+      positiveThree.unshift(arrayOfInts[i]);
+    } else if (arrayOfInts[i] >= positiveThree[1]) {
+      positiveThree.splice(1, 0, arrayOfInts[i]);
+    } else if (arrayOfInts[i] >= positiveThree[2]) {
+      positiveThree.splice(2, 0, arrayOfInts[i]);
+    }
+    if (positiveThree.length > 3) positiveThree.pop();
 
+    if (arrayOfInts[i] < 0) {
+      if (arrayOfInts[i] < negativeTwo[0]) {
+        negativeTwo.unshift(arrayOfInts[i]);
+      } else if (arrayOfInts[i] < negativeTwo[1]) {
+        negativeTwo.pop();
+        negativeTwo.push(arrayOfInts[i]);
+      }
+    }
+  }
+
+  return Math.max(
+    positiveThree[0] * positiveThree[1] * positiveThree[2],
+    positiveThree[0] * negativeTwo[0] * negativeTwo[1]);
 }
