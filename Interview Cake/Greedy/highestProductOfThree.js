e89167dce80665ebc0494471fa83a8a0a0a41ee8
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-01 13:41:21 -0600

    init

---
+++ Interview Cake/Greedy/highestProductOfThree.js
@@ -0,0 +1,9 @@
+/*
+Given an array of integers, find the highest product you can get from three of the integers.
+The input arrayOfInts will always have at least three integers.
+*/
+
+function highestProductOf3(arrayOfInts) {
+
+
+}
