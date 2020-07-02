5f396c92e13dfd3d60fcfcbf18f732c5b0eca8d6
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-02 12:04:23 -0600

    First pass without tests.

--- LeetCode/July/Week1/levelOrderBottom.js
+++ LeetCode/July/Week1/levelOrderBottom.js
@@ -19,7 +19,7 @@ return its bottom-up level order traversal as:
 ]
 */
 
-/**
+/*
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
@@ -27,10 +27,26 @@ return its bottom-up level order traversal as:
  *     this.right = (right===undefined ? null : right)
  * }
  */
+
 /*
  * @param {TreeNode} root
  * @return {number[][]}
  */
 var levelOrderBottom = function(root) {
+  let result = [];
 
+  let stack = [[root, 0]];
+
+  while (stack.length) {
+    let [node, depth] = stack.pop();
+
+    result[depth] === undefined
+    ? result[depth] = [node.val]
+    : result[depth].push(node.val);
+
+    if (node.left !== null) stack.push([node.left, depth + 1]);
+    if (node.right !== null) stack.push([node.right, depth + 1]);
+  }
+
+  return result.reverse();
 };
