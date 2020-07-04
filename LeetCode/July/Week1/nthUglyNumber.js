cb41842a590e3fc6ceca11d113d2a6f1ac8e0df1
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-04 09:12:38 -0600

    First pass without tests.

--- LeetCode/July/Week1/nthUglyNumber.js
+++ LeetCode/July/Week1/nthUglyNumber.js
@@ -19,5 +19,37 @@ Note:
  */
 
 var nthUglyNumber = function(n) {
+  if (n < 0) return 0;
+  // if (n === 1) return 1;
 
+  let uglyNumber = 1;
+  let count = 1;
+
+  let ugly = [1, 1, 1];
+
+  while (count <= n) {
+    let [twoMult, threeMult, fiveMult] = ugly;
+
+    uglyNumber = Math.min(
+      2 * twoMult,
+      3 * threeMult,
+      5 * fiveMult
+    );
+
+    if (2 * twoMult === uglyNumber) {
+      ugly[0] += 1;
+    }
+
+    if (3 * threeMult === uglyNumber) {
+      ugly[1] += 1;
+    }
+
+    if (5 * fiveMult === uglyNumber) {
+      ugly[2] += 1;
+    }
+
+    count += 1;
+  }
+
+  return uglyNumber;
 };
