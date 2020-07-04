86a5305385858e2a7cbbac2e75ad2c6834ecb358
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-04 10:30:31 -0600

    Refactor and solving.

--- LeetCode/July/Week1/nthUglyNumber.js
+++ LeetCode/July/Week1/nthUglyNumber.js
@@ -19,37 +19,62 @@ Note:
  */
 
 var nthUglyNumber = function(n) {
-  if (n < 0) return 0;
-  // if (n === 1) return 1;
+  if (n <= 0) return 0;
 
-  let uglyNumber = 1;
+
+  let ugly = [0, 0, 0];
+
+  let uglyNumbers = [1];
   let count = 1;
 
-  let ugly = [1, 1, 1];
+  while (count < n) {
+    let [twos, threes, fives] = ugly;
 
-  while (count <= n) {
-    let [twoMult, threeMult, fiveMult] = ugly;
-
-    uglyNumber = Math.min(
-      2 * twoMult,
-      3 * threeMult,
-      5 * fiveMult
+    uglyNumbers[count] = Math.min(
+      uglyNumbers[twos] * 2,
+      Math.min(
+        uglyNumbers[threes] * 3,
+        uglyNumbers[fives] * 5,
+      )
     );
 
-    if (2 * twoMult === uglyNumber) {
+
+    if (uglyNumbers[count] === uglyNumbers[twos] * 2) {
       ugly[0] += 1;
     }
 
-    if (3 * threeMult === uglyNumber) {
+    if (uglyNumbers[count] === uglyNumbers[threes] * 3) {
       ugly[1] += 1;
     }
 
-    if (5 * fiveMult === uglyNumber) {
+    if (uglyNumbers[count] === uglyNumbers[fives] * 5) {
       ugly[2] += 1;
     }
 
     count += 1;
+    // console.log('uglyNumber: ', uglyNumber, 'count: ', count);
   }
 
-  return uglyNumber;
+  return uglyNumbers[uglyNumbers.length - 1];
 };
+
+///////////// TESTS /////////////
+
+// should work on the example
+console.log(nthUglyNumber(10) === 12);
+
+// should return 0 with 0
+console.log(nthUglyNumber(0) === 0);
+
+// should return 1 with 1
+console.log(nthUglyNumber(1) === 1);
+// console.log(nthUglyNumber(0));
+
+// should return zero with a negative number
+console.log(nthUglyNumber(-2) === 0);
+
+// should work with larger numbers
+console.log(nthUglyNumber(100) === 1536);
+
+// should be able to handle the largest number passed in
+console.log(nthUglyNumber(1690) === 2123366400);
