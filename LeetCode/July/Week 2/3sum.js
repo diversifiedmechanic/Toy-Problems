93340fc6985a9178caf18270a166f1922430164e
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-08 09:59:53 -0600

    Solves but too time complex.

--- LeetCode/July/Week 2/3sum.js
+++ LeetCode/July/Week 2/3sum.js
@@ -19,10 +19,10 @@ A solution set is:
 */
 
 var threeSum = function(nums) {
-  let result = [];
+  let resultSet = new Set();
   let store = {};
 
-  for (let num in nums) {
+  for (let num of nums) {
     if (store[num] === undefined) {
       store[num] = 1;
     } else {
@@ -34,20 +34,50 @@ var threeSum = function(nums) {
     for (let j = i + 1; j < nums.length; j++) {
       let totalOfTwo = nums[i] + nums[j];
       if (store[totalOfTwo * -1] !== undefined) {
+        if (nums[i] === nums[j] && store[nums[i]] === 2) break;
+
         let validFirst = true;
         let validSecond = true;
-        if (store[totalOfTwo * -1] === nums[i] && store[nums[i] === 1]) {
+
+        if ((totalOfTwo * -1) === nums[i] && store[nums[i]] === 1) {
           validFirst = false;
         }
-        if (store[totalOfTwo * -1] === nums[j] && store[nums[j] === 1]) {
+        if ((totalOfTwo * -1) === nums[j] && store[nums[j]] === 1) {
           validSecond = false;
         }
         if (validFirst && validSecond) {
-          result.push([nums[i], nums[j], store[totalOfTwo * -1]])
+          resultSet.add(JSON.stringify([nums[i], nums[j], totalOfTwo * -1].sort()));
         }
       }
     }
   }
 
+  let result = [];
+
+  for (let [set] of resultSet.entries()) {
+    result.push(JSON.parse(set));
+  }
+
   return result;
 };
+
+////////// TESTS //////////////
+
+var nums = [-1, 0, 1, 2, -1, -4];
+var expected = [
+  [-1, 0, 1],
+  [-1, -1, 2]
+];
+// should solve the example
+console.log(JSON.stringify(threeSum(nums)));
+
+
+var nums = [0,0,0];
+var expected = [0,0,0];
+// should return zeros if they are all there
+console.log(JSON.stringify(threeSum(nums)));
+
+var nums = [0,0];
+var expected = [];
+// should return return an empty array if no matches
+console.log(JSON.stringify(threeSum(nums)));
