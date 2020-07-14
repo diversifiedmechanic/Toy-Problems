4cb1ee26565002c3e3c104a5eeb3871976bd2d94
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-14 09:25:18 -0600

    Solving all tests.

--- LeetCode/July/Week 2/angleClock.js
+++ LeetCode/July/Week 2/angleClock.js
@@ -29,8 +29,25 @@ Output: 0
  */
 
 var angleClock = function(hour, minutes) {
+  hour = hour === 12 ? 0 : hour;
+
   let hourHand = (hour * 30) + (minutes * .5);
   let minuteHand = minutes * 6;
 
-  return Math.abs(hourHand - minuteHand);
+  let angle = Math.abs(hourHand - minuteHand);
+
+  return angle > 180 ? 360 - angle : angle;
 };
+
+/////////////// TESTS /////////////
+console.log(angleClock(12, 30) === 165);
+
+console.log(angleClock(3, 30) === 75);
+
+console.log(angleClock(3, 15) === 7.5);
+
+console.log(angleClock(4, 50) === 155);
+
+console.log(angleClock(12, 0) === 0);
+
+console.log(angleClock(1, 57) === 76.5);
