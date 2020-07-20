7b9153b00dabdeffb61a9962882a36deff0577b9
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-20 10:53:31 -0600

    init

--- LeetCode/July/Week 3/removeElements.js
+++ LeetCode/July/Week 3/removeElements.js
@@ -5,3 +5,21 @@ Example:
 Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 */
+
+/*
+* Definition for singly-linked list.
+* function ListNode(val, next) {
+*     this.val = (val===undefined ? 0 : val)
+*     this.next = (next===undefined ? null : next)
+* }
+*/
+
+/*
+* @param {ListNode} head
+* @param {number} val
+* @return {ListNode}
+*/
+
+var removeElements = function(head, val) {
+
+};
