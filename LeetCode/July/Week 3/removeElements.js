a7cf18c8c3b9499497b428dc11cd9be8c312f291
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-20 11:04:56 -0600

    First pass with helper functions built.

--- LeetCode/July/Week 3/removeElements.js
+++ LeetCode/July/Week 3/removeElements.js
@@ -21,5 +21,38 @@ Output: 1->2->3->4->5
 */
 
 var removeElements = function(head, val) {
+  let start = head;
+  let prev = null;
 
+  while (start) {
+    if (start.val === val) {
+      if (prev !== null) {
+        prev = start.next;
+        start = start.next;
+      } else {
+        head = head.next;
+      }
+    }
+  }
+
+  return head;
 };
+
+//////////// HELPERS ////////////
+const ListNode = function(val, next) {
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
+};
+
+const buildLinkedList = function(arr) {
+  let list = new ListNode(arr[0]);
+  let head = list;
+
+  for (let i = 1; i < arr.length; i++) {
+    list.next = new ListNode(arr[i]);
+    list = list.next;
+  }
+
+  return head;
+};
+
