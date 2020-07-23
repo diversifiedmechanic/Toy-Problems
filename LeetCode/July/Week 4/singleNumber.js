18272427c7ada1725886b5bef80f510e9449ee6f
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-23 09:16:01 -0600

    init

---
+++ LeetCode/July/Week 4/singleNumber.js
@@ -0,0 +1,19 @@
+/*
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once.
+
+Example:
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+
+Note:
+    The order of the result is not important. So in the above example, [5, 3] is also correct.
+*/
+
+/*
+* @param {number[]} nums
+* @return {number[]}
+*/
+
+var singleNumber = function(nums) {
+
+};
