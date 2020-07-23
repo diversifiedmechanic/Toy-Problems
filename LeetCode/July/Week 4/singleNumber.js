38a0346fdf3e8d6358ba1692fd7e7edb6213aed2
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-23 09:18:03 -0600

    First pass without tests.

--- LeetCode/July/Week 4/singleNumber.js
+++ LeetCode/July/Week 4/singleNumber.js
@@ -15,5 +15,16 @@ Note:
 */
 
 var singleNumber = function(nums) {
+  let set = new Set();
 
+  nums.forEach((num) => {
+    if (set.has(num)) {
+      set.delete(num);
+    } else {
+      set.add(num);
+    }
+  });
+
+  return Array.from(set);
 };
+
