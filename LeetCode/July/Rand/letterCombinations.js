5020fa6b5c0f73e84bf5db2dc86ab33c316e043b
wsamsnyder <diversifiedmechanic01@gmail.com>
2020-07-29 10:38:31 -0600

    Added number to letter mapping.

--- LeetCode/July/Rand/letterCombinations.js
+++ LeetCode/July/Rand/letterCombinations.js
@@ -16,6 +16,27 @@ Although the above answer is in lexicographical order, your answer could be in a
  * @return {string[]}
  */
 
+var numToLetters = (num) => {
+  switch(num) {
+    case '2':
+      return 'abc';
+    case '3':
+      return 'def';
+    case '4':
+      return 'ghi';
+    case '5':
+      return 'jkl';
+    case '6':
+      return 'mno';
+    case '7':
+      return 'pqrs';
+    case '8':
+      return 'tuv';
+    case '9':
+      return 'wxyz';
+  }
+};
+
 var letterCombinations = function(digits) {
 
 };
